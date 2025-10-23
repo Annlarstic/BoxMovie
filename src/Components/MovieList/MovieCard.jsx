fc7d822640e1da300d20cb8629b0266208b6db0a
Annlarstic <nonsochi1823@gmail.com>
2025-10-23 01:36:20 +0100

    MovieBox

--- src/Components/MovieList/MovieCard.jsx
+++ src/Components/MovieList/MovieCard.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import "./MovieCard.css"
-// import Star from "../../assets/emoji/star.jpg"
-
-// function MovieCard(movie) {
-//   return (
-//     <a href="" className="mobie_card">
-//         <img className="movie_poster" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/adventure-movie-poster-template-design-7b13ea2ab6f64c1ec9e1bb473f345547_screen.jpg?ts=1636999411" alt="" />
-
-
-// <div className="movie_details">
-//     <h3 className="movie_details_header">{movie.original_title}</h3>
-//     <div className="movie_date_rate">
-//         <p>{movie.release_date}</p>
-//         <p> {"movie.rate_average "} <img className="rating_emoji" src={Star} alt="" /></p>
-//     </div>
-//     <p className="description">{movie.overview.slice(0, 120)  + "..."}</p>
-// </div>
-
-//     </a>
-//   )
-// }
-
-// export default MovieCard
-
-
-
-
-
-
 import React from "react";
 import "./MovieCard.css"
 import Star from "../../assets/emoji/star.jpg"
